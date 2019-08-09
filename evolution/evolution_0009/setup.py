440f5875af2c53aaebf7435ace3da8b7268488c9
daocloud <daocloud@huzhideMacBook-Pro.local>
2019-08-09 18:09:41 +0800

    add setup.py script

--- evolution/evolution_0009/setup.py
+++ evolution/evolution_0009/setup.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+standard commands (标准命令)是 distutils 内建命令，而 Extra commands（附加命令）是像 setuptools 这样的第三方包创建的
+python setup.py install -n --record install.log
+python setup.py develop
+"""
 
 from distutils.core import setup
-import bottle
+import setuptools
 
-setup(name='Bottle',
-      version='%d.%d.%d' % bottle.__version__,
+import evolution_0009
+
+setup(name='evolution_0009',
+      version='%d.%d.%d' % evolution_0009.__version__,
       description='Bottle Web Framework',
-      long_description=open('README.md').read(),
+      long_description=open('../../README.md').read(),
       author='Marcel Hellkamp',
       author_email='marc@gsites.de',
       url='http://github.com/defnull/bottle',
-      py_modules=['bottle'],
+      py_modules=['evolution_0009'],
       license='MIT',
       classifiers=['Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
