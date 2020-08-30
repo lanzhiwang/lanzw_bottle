1667e0f567af48bcea76dac01f569885e89a2b65
lanzhiwang <hzhilamp@163.com>
2020-08-30 15:09:09 +0800

    evolution 04

--- README.md
+++ README.md
@@ -35,6 +35,6 @@ lanzw.py 文件汇总了整个框架的功能。
 
 https://github.com/bottlepy/bottle/commits/master?after=357a0cb39cb8337f8467f5396e4b7caaa7e4f25c+839&path%5B%5D=bottle.py
 
-Use StringIO instead of tempfile for small POST requests.
+Reusable Router classes and speedup for dynamic routes.
 
 git checkout -b evolution_02 444443ce1d863dbb91e141891f7f514865dcae61
---
+++ evolution/evolution_04/test/testall.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import unittest
+import sys
+import test_templates
+import test_stpl
+import test_mako
+import test_jinja2
+import test_routes
+import test_environ
+import test_wsgi
+import test_sendfile
+
+suite = unittest.TestSuite()
+suite.addTest(test_stpl.suite)
+suite.addTest(test_mako.suite)
+suite.addTest(test_jinja2.suite)
+suite.addTest(test_templates.suite)
+suite.addTest(test_routes.suite)
+suite.addTest(test_environ.suite)
+suite.addTest(test_wsgi.suite)
+suite.addTest(test_sendfile.suite)
+
+if __name__ == '__main__':
+    result = unittest.TextTestRunner(verbosity=0).run(suite)
+    sys.exit((result.errors or result.failures) and 1 or 0)
+
